eaf71a7d0c3fbc5302b0ce3c43f32665d6fea607
San <san.robinxin@gmail.com>
2024-02-05 08:54:48 +0800

    update json

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,13 @@ import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-	experimental: {
-		mdxRs: true,
-	},
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  experimental: {
+    mdxRs: true,
+    images: {
+      // Adjust the images configuration as needed
+    },
+  },
 };
 
 export default withContentlayer(nextConfig);
